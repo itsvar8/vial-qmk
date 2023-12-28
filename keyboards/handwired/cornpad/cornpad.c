6fc6012bf28deef467af3729feccc7d8c6bc0939
Var <120035196+itsvar8@users.noreply.github.com>
2023-12-28 18:52:17 +0100

    keymap and rgb update

--- keyboards/handwired/cornpad/cornpad.c
+++ keyboards/handwired/cornpad/cornpad.c
@@ -8,16 +8,16 @@
 led_config_t g_led_config = {
 {
 // Key Matrix to LED Index
-  { NO_LED, NO_LED, NO_LED, NO_LED, NO_LED,         0, NO_LED, NO_LED, NO_LED,    NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
-  { NO_LED, NO_LED, NO_LED, NO_LED, NO_LED,    NO_LED, NO_LED, NO_LED, NO_LED,    NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
-  { NO_LED, NO_LED, NO_LED, NO_LED, NO_LED,    NO_LED, NO_LED, NO_LED, NO_LED,    NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
-  { NO_LED, NO_LED, NO_LED, NO_LED, NO_LED,    NO_LED, NO_LED, NO_LED, NO_LED,    NO_LED, NO_LED, NO_LED, NO_LED, NO_LED }
+  { NO_LED, NO_LED,      3,      2,      1,         0, NO_LED, NO_LED,     20,        19,     18,     17, NO_LED, NO_LED },
+  { NO_LED, NO_LED,      4,     31,     32,        33, NO_LED, NO_LED,     21,        22,     23,     16, NO_LED, NO_LED },
+  { NO_LED, NO_LED,      5,     30,     29,        28, NO_LED,     27,     26,        25,     24,     15, NO_LED, NO_LED },
+  { NO_LED, NO_LED,      6,      7,      8,         9,     10, NO_LED,     11,        12,     13,     14, NO_LED, NO_LED }
 }, {
 // LED Index to Physical Position
-	{ 104, 0}
+	{ 90, 11}, { 67, 9}, { 45, 7}, { 23, 3}, { 7, 17}, { 8, 35}, { 14, 53}, { 38, 57}, { 63, 60}, { 87, 61}, { 112, 62}, { 137, 61}, { 161, 60}, { 186, 57}, { 210, 53}, { 216, 35}, { 217, 17}, { 201, 3}, { 179, 7}, { 157, 9}, { 134, 11}, { 138, 28}, { 165, 26}, { 191, 22}, { 190, 39}, { 164, 42}, { 138, 44}, { 112, 45}, { 86, 44}, { 60, 42}, { 34, 39}, { 33, 22}, { 59, 26}, { 86, 28}
 }, {
 // LED Index to Flag
-  4
+  4, 4, 4, 4, 4, 4, 4, 1, 1, 4, 4, 4, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4
 }
 };
 
@@ -25,32 +25,43 @@ led_config_t g_led_config = {
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 	for (uint8_t i = led_min; i < led_max; i++) {
-		switch(get_highest_layer(layer_state)) {
-			case 1:
-				rgb_matrix_set_color(i, RGB_RED);
-				break;
-			case 2:
-				rgb_matrix_set_color(i, RGB_YELLOW);
-				break;
-			case 3:
-				rgb_matrix_set_color(i, RGB_BLUE);
-				break;
-			case 4:
-				rgb_matrix_set_color(i, RGB_MAGENTA);
-				break;
-			case 5:
-				rgb_matrix_set_color(i, RGB_TEAL);
-				break;
-			case 6:
-				rgb_matrix_set_color(i, RGB_ORANGE);
-				break;
-			case 7:
-				rgb_matrix_set_color(i, RGB_PURPLE);
-				break;
-
-			default:
-				break;
+		if (g_led_config.flags[i] & LED_FLAG_MODIFIER) {
+			switch(get_highest_layer(layer_state)) {
+				case 1:
+					rgb_matrix_set_color(i, RGB_RED);
+					break;
+				case 2:
+					rgb_matrix_set_color(i, RGB_YELLOW);
+					break;
+				case 3:
+					rgb_matrix_set_color(i, RGB_BLUE);
+					break;
+				case 4:
+					rgb_matrix_set_color(i, RGB_MAGENTA);
+					break;
+				case 5:
+					rgb_matrix_set_color(i, RGB_TEAL);
+					break;
+				case 6:
+					rgb_matrix_set_color(i, RGB_ORANGE);
+					break;
+				case 7:
+					rgb_matrix_set_color(i, RGB_PURPLE);
+					break;
+				default:
+					break;
+			}
 		}
     }
 	return false;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+	setPinOutput(25);
+    if (get_highest_layer(state) > 0) {
+        writePinHigh(25);
+    } else {
+        writePinLow(25);
+	}
+  return state;
 }
--- keyboards/handwired/cornpad/keymaps/vial/keymap.c
+++ keyboards/handwired/cornpad/keymaps/vial/keymap.c
@@ -3,22 +3,89 @@
 
 #include QMK_KEYBOARD_H
 
+bool is_alt_tab_active = false; // ADD this near the beginning of keymap.c
+uint16_t alt_tab_timer = 0;     // we will be using them soon.
+
+enum custom_keycodes {          // Make sure have the awesome keycode ready
+  ALT_TAB = QK_KB_0,
+  INV_ALT_TAB,
+  ALT_ESC,
+  XPLR,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) { // This will do most of the grunt work with the keycodes.
+    case ALT_TAB:
+      if (record->event.pressed) {
+        if (!is_alt_tab_active) {
+          is_alt_tab_active = true;
+          register_code(KC_LALT);
+        }
+        alt_tab_timer = timer_read();
+        register_code(KC_TAB);
+      } else {
+        unregister_code(KC_TAB);
+      }
+      break;
+	case INV_ALT_TAB:
+      if (record->event.pressed) {
+        if (!is_alt_tab_active) {
+          is_alt_tab_active = true;
+          register_code(KC_LALT);
+        }
+        alt_tab_timer = timer_read();
+		register_code(KC_LSFT);
+        register_code(KC_TAB);
+      } else {
+        unregister_code(KC_LSFT);
+        unregister_code(KC_TAB);
+      }
+      break;
+	case ALT_ESC:
+	  if (record->event.pressed) {
+		  register_code(KC_LALT);
+		  tap_code(KC_ESC);
+	  } else {
+		unregister_code(KC_LALT);
+	  }
+	  break;
+	case XPLR:
+	  if (record->event.pressed) {
+		  register_code(KC_LGUI);
+		  tap_code(KC_E);
+	  } else {
+		unregister_code(KC_LGUI);
+	  }
+	  break;
+  }
+  return true;
+}
+
+void matrix_scan_user(void) { // The very important timer.
+  if (is_alt_tab_active) {
+    if (timer_elapsed(alt_tab_timer) > 800) {
+      is_alt_tab_active = false;
+	  unregister_code(KC_LALT);
+    }
+  }
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_default(
-  //,--------------------------------------------.            ,----------------------------------.            ,--------------------------------------------.
-         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_P7,   KC_P8,   KC_P9, KC_PSLS,                 KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-  //|--------+--------+--------+--------+--------|            +-------+--------+--------+--------+            |--------+--------+--------+--------+--------|
-         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_P4,   KC_P5,   KC_P6, KC_PAST,                 KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,
-  //|--------+--------+--------+--------+--------|            +-------+--------+--------+--------+            |--------+--------+--------+--------+--------|
-         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_P1,   KC_P2,   KC_P3, KC_PMNS,                 KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
-  //|--------+--------+--------+--------+--------+--------|   +-------+--------+--------+--------+   |--------+--------+--------+--------+--------+--------|
-                         KC_ESC, TL_LOWR,  KC_SPC,  KC_GRV,      KC_P0, KC_PDOT, KC_PENT, KC_PPLS,     KC_QUOT, KC_BSPC, TL_UPPR,  KC_ENT
-                    //`-----------------------------------'   `----------------------------------'   `-----------------------------------'
+  //,---------------------------------------------------------------.            ,----------------------------------.            ,-------------------------------------------------------------------.
+             KC_Q,         KC_W,         KC_E,         KC_R,    KC_T,               KC_P7,   KC_P8,   KC_P9, KC_PSLS,                 KC_Y,         KC_U,         KC_I,         KC_O,            KC_P,
+  //|------------+-------------+-------------+-------------+--------|            +-------+--------+--------+--------+            |--------+-------------+-------------+-------------+----------------|
+     LSFT_T(KC_A), LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F),    KC_G,               KC_P4,   KC_P5,   KC_P6, KC_PAST,                 KC_H, LGUI_T(KC_J), LALT_T(KC_K), LCTL_T(KC_L), LSFT_T(KC_SCLN),
+  //|------------+-------------+-------------+-------------+--------|            +-------+--------+--------+--------+            |--------+-------------+-------------+-------------+----------------|
+             KC_Z,         KC_X,         KC_C,         KC_V,    KC_B,               KC_P1,   KC_P2,   KC_P3, KC_PMNS,                 KC_N,         KC_M,      KC_COMM,       KC_DOT,         KC_SLSH,
+  //|------------+-------------+-------------+-------------+--------+--------|   +-------+--------+--------+--------+   |--------+--------+-------------+-------------+-------------+----------------|
+                                      QK_GESC,      TL_LOWR,  KC_SPC,    XPLR,      KC_P0, KC_PDOT, KC_PENT, KC_PPLS,     ALT_ESC, KC_BSPC,      TL_UPPR,       KC_ENT
+                                   //`---------------------------------------'   `----------------------------------'   `---------------------------------------------'|
   ),
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] = {  ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [0] = {  ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(INV_ALT_TAB, ALT_TAB)  },
 };
 #endif
