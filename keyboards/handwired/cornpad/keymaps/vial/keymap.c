1e9bf654f9b2d73447852af7fc2b3935630603d4
Var <120035196+itsvar8@users.noreply.github.com>
2024-01-02 22:00:17 +0100

    Revert "midi+debounce+tweaks"
    This reverts commit ff6989cc7fc2c98f6f890c5861bba814439ebf96.

--- keyboards/handwired/cornpad/keymaps/vial/keymap.c
+++ keyboards/handwired/cornpad/keymaps/vial/keymap.c
@@ -7,17 +7,11 @@ bool is_alt_tab_active = false; // ADD this near the beginning of keymap.c
 uint16_t alt_tab_timer = 0;     // we will be using them soon.
 
 enum custom_keycodes {          // Make sure have the awesome keycode ready
-  CYCLE_LAYERS = QK_KB_0,
-  ALT_TAB,
+  ALT_TAB = QK_KB_0,
   INV_ALT_TAB,
   ALT_ESC,
   XPLR,
 };
-
-// 1st layer on the cycle
-#define LAYER_CYCLE_START 0
-// Last layer on the cycle
-#define LAYER_CYCLE_END   7
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) { // This will do most of the grunt work with the keycodes.
@@ -63,30 +57,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 		unregister_code(KC_LGUI);
 	  }
 	  break;
-	case CYCLE_LAYERS:
-      // Our logic will happen on presses, nothing is done on releases
-      if (!record->event.pressed) { 
-        // We've already handled the keycode (doing nothing), let QMK know so no further code is run unnecessarily
-        return false;
-      }
-
-      uint8_t current_layer = get_highest_layer(layer_state);
-
-      // Check if we are within the range, if not quit
-      if (current_layer > LAYER_CYCLE_END || current_layer < LAYER_CYCLE_START) {
-        return false;
-      }
-
-      uint8_t next_layer = current_layer + 1;
-      if (next_layer > LAYER_CYCLE_END) {
-          next_layer = LAYER_CYCLE_START;
-      }
-      layer_move(next_layer);
-      return false;
-
-    // Process other keycodes normally
-    default:
-      return true;
   }
   return true;
 }
@@ -98,15 +68,6 @@ void matrix_scan_user(void) { // The very important timer.
 	  unregister_code(KC_LALT);
     }
   }
-}
-
-void keyboard_post_init_user(void) {
-    vial_tap_dance_entry_t td = { CYCLE_LAYERS,
-                                  TO(0),
-                                  KC_NO,
-                                  KC_NO,
-                                  TAPPING_TERM };
-    dynamic_keymap_set_tap_dance(0, &td); // the first value corresponds to the TD(i) slot
 }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -121,46 +82,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       QK_GESC,      TL_LOWR,  KC_SPC,    XPLR,      KC_P0, KC_PDOT, KC_PENT, KC_PPLS,     ALT_ESC, KC_BSPC,      TL_UPPR,       KC_ENT
                                    //`---------------------------------------'   `----------------------------------'   `---------------------------------------------'|
   ),
-    [1] = LAYOUT_default(
-  //,---------------------------------------------------------------.            ,----------------------------------.            ,-------------------------------------------------------------------.
-           KC_DEL,      KC_TRNS,      KC_TRNS,      KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,         KC_TRNS,
-  //|------------+-------------+-------------+-------------+--------|            +-------+--------+--------+--------+            |--------+-------------+-------------+-------------+----------------|
-          KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,         KC_TRNS,
-  //|------------+-------------+-------------+-------------+--------|            +-------+--------+--------+--------+            |--------+-------------+-------------+-------------+----------------|
-          KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,         KC_TRNS,
-  //|------------+-------------+-------------+-------------+--------+--------|   +-------+--------+--------+--------+   |--------+--------+-------------+-------------+-------------+----------------|
-                                      KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,      KC_TRNS,      KC_TRNS
-                                   //`---------------------------------------'   `----------------------------------'   `---------------------------------------------'|
-  ),
-    [2] = LAYOUT_default(
-  //,---------------------------------------------------------------.            ,----------------------------------.            ,-------------------------------------------------------------------.
-          KC_TRNS,        KC_UP,      KC_TRNS,      KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,         KC_TRNS,
-  //|------------+-------------+-------------+-------------+--------|            +-------+--------+--------+--------+            |--------+-------------+-------------+-------------+----------------|
-          KC_LEFT,      KC_DOWN,     KC_RIGHT,      KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,         KC_TRNS,
-  //|------------+-------------+-------------+-------------+--------|            +-------+--------+--------+--------+            |--------+-------------+-------------+-------------+----------------|
-          KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,         KC_TRNS,
-  //|------------+-------------+-------------+-------------+--------+--------|   +-------+--------+--------+--------+   |--------+--------+-------------+-------------+-------------+----------------|
-                                      KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,      KC_TRNS,      KC_TRNS
-                                   //`---------------------------------------'   `----------------------------------'   `---------------------------------------------'|
-  ),
-    [3] = LAYOUT_default(
-  //,---------------------------------------------------------------.            ,----------------------------------.            ,-------------------------------------------------------------------.
-          QK_BOOT,      KC_TRNS,      KC_TRNS,      KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,         KC_TRNS,
-  //|------------+-------------+-------------+-------------+--------|            +-------+--------+--------+--------+            |--------+-------------+-------------+-------------+----------------|
-          KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,         KC_TRNS,
-  //|------------+-------------+-------------+-------------+--------|            +-------+--------+--------+--------+            |--------+-------------+-------------+-------------+----------------|
-          KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,         KC_TRNS,
-  //|------------+-------------+-------------+-------------+--------+--------|   +-------+--------+--------+--------+   |--------+--------+-------------+-------------+-------------+----------------|
-                                      KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,      KC_TRNS,      KC_TRNS
-                                   //`---------------------------------------'   `----------------------------------'   `---------------------------------------------'|
-  ),
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [0] = {  ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(INV_ALT_TAB, ALT_TAB)  },
-	[1] = {  ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(INV_ALT_TAB, ALT_TAB)  },
-	[2] = {  ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(INV_ALT_TAB, ALT_TAB)  },
-	[3] = {  ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(INV_ALT_TAB, ALT_TAB)  },
 };
 #endif
