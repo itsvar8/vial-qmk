e6961bf8b22a81846bc54ea2bd0084f3d44307eb
Var <120035196+itsvar8@users.noreply.github.com>
2024-03-10 11:34:59 +0100

    keymap fixes + MIDI CC

--- keyboards/5_4_macropad/keymaps/vial/keymap.c
+++ keyboards/5_4_macropad/keymaps/vial/keymap.c
@@ -3,8 +3,12 @@
 
 #include QMK_KEYBOARD_H
 
+extern MidiDevice midi_device;
+
 bool is_alt_tab_active = false; // ADD this near the beginning of keymap.c
 uint16_t alt_tab_timer = 0;     // we will be using them soon.
+
+int8_t current_MIDI_cc = 0;
 
 enum custom_keycodes {          // Make sure have the awesome keycode ready
   NEXT_LAYER = QK_KB_0,
@@ -19,7 +23,15 @@ enum custom_keycodes {          // Make sure have the awesome keycode ready
   SCR_REC,
   CLR_EPR,
   CLR_KBD,
+  MIDI_UP,
+  MIDI_DN,
+  CC_RST,
+  CC_UP,
+  CC_DN,
 };
+
+#define MIDI_CYCLE_START 0
+#define MIDI_CYCLE_END   63
 
 // 1st layer on the cycle
 #define LAYER_CYCLE_START 0
@@ -28,6 +40,44 @@ enum custom_keycodes {          // Make sure have the awesome keycode ready
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) { // This will do most of the grunt work with the keycodes.
+	case NEXT_LAYER:
+      // Our logic will happen on presses, nothing is done on releases
+      if (!record->event.pressed) { 
+        // We've already handled the keycode (doing nothing), let QMK know so no further code is run unnecessarily
+        return false;
+      }
+      uint8_t current_layer = get_highest_layer(layer_state);
+
+      // Check if we are within the range, if not quit
+      if (current_layer > LAYER_CYCLE_END || current_layer < LAYER_CYCLE_START) {
+        return false;
+      }
+
+      uint8_t next_layer = current_layer + 1;
+      if (next_layer > LAYER_CYCLE_END) {
+          next_layer = LAYER_CYCLE_START;
+      }
+      layer_move(next_layer);
+      break;
+	case PREV_LAYER:
+      // Our logic will happen on presses, nothing is done on releases
+      if (!record->event.pressed) { 
+        // We've already handled the keycode (doing nothing), let QMK know so no further code is run unnecessarily
+        return false;
+      }
+	  int8_t current_layer2 = get_highest_layer(layer_state);
+
+      // Check if we are within the range, if not quit
+      if (current_layer2 > LAYER_CYCLE_END || current_layer2 < LAYER_CYCLE_START) {
+        return false;
+      }
+
+      int8_t prev_layer = current_layer2 - 1;
+      if (prev_layer < LAYER_CYCLE_START) {
+          prev_layer = LAYER_CYCLE_END;
+      }
+      layer_move(prev_layer);
+	  break;
     case ALT_TAB:
       if (record->event.pressed) {
         if (!is_alt_tab_active) {
@@ -118,48 +168,48 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	  } else {
 	  }
 	  break;
-	case NEXT_LAYER:
-      // Our logic will happen on presses, nothing is done on releases
-      if (!record->event.pressed) { 
-        // We've already handled the keycode (doing nothing), let QMK know so no further code is run unnecessarily
-        return false;
+	case MIDI_UP:
+	  if (record->event.pressed) {
+          midi_send_cc(&midi_device, 0, current_MIDI_cc, 65);
+	  } else {
+	  }
+	  break;
+	case MIDI_DN:
+	  if (record->event.pressed) {
+          midi_send_cc(&midi_device, 0, current_MIDI_cc, 63);
+	  } else {
+	  }
+	  break;
+	case CC_RST:
+      if (record->event.pressed) {
+          current_MIDI_cc = MIDI_CYCLE_START;
+      } else {
       }
-      uint8_t current_layer = get_highest_layer(layer_state);
-
-      // Check if we are within the range, if not quit
-      if (current_layer > LAYER_CYCLE_END || current_layer < LAYER_CYCLE_START) {
-        return false;
+      break;
+	case CC_UP:
+      if (record->event.pressed) {
+          if (current_MIDI_cc > MIDI_CYCLE_END || current_MIDI_cc < MIDI_CYCLE_START) {
+            return false;
+          } else {
+			  current_MIDI_cc++;
+          }
+	  }
+	  if (current_MIDI_cc > MIDI_CYCLE_END) {
+          current_MIDI_cc = MIDI_CYCLE_START;
       }
-
-      uint8_t next_layer = current_layer + 1;
-      if (next_layer > LAYER_CYCLE_END) {
-          next_layer = LAYER_CYCLE_START;
+      break;
+	case CC_DN:
+      if (record->event.pressed) {
+          if (current_MIDI_cc > MIDI_CYCLE_END || current_MIDI_cc < MIDI_CYCLE_START) {
+            return false;
+          } else {
+			  current_MIDI_cc--;
+          }
+	  }
+	  if (current_MIDI_cc < MIDI_CYCLE_START) {
+          current_MIDI_cc = MIDI_CYCLE_END;
       }
-      layer_move(next_layer);
-      return false;
-	case PREV_LAYER:
-      // Our logic will happen on presses, nothing is done on releases
-      if (!record->event.pressed) { 
-        // We've already handled the keycode (doing nothing), let QMK know so no further code is run unnecessarily
-        return false;
-      }
-	  int8_t current_layer2 = get_highest_layer(layer_state);
-
-      // Check if we are within the range, if not quit
-      if (current_layer2 > LAYER_CYCLE_END || current_layer2 < LAYER_CYCLE_START) {
-        return false;
-      }
-
-      int8_t prev_layer = current_layer2 - 1;
-      if (prev_layer < LAYER_CYCLE_START) {
-          prev_layer = LAYER_CYCLE_END;
-      }
-      layer_move(prev_layer);
-      return false;
-
-    // Process other keycodes normally
-    default:
-      return true;
+      break;
   }
   return true;
 }
