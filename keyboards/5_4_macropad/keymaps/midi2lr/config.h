4c5f60174d12e8e7059f31f59300b6fb3035ef8f
Var <120035196+itsvar8@users.noreply.github.com>
2024-03-13 17:51:16 +0100

    midi2lr keymap added
    needs vial-gui pr #229

---
+++ keyboards/5_4_macropad/keymaps/midi2lr/config.h
@@ -0,0 +1,11 @@
+// Copyright 2024 Var (@itsvar8)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x53, 0xC0, 0x70, 0x17, 0xD0, 0xB8, 0x92, 0x14}
+#define VIAL_UNLOCK_COMBO_ROWS { 1, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
+#define DYNAMIC_KEYMAP_MACRO_COUNT 32
+#define VIAL_TAP_DANCE_ENTRIES 32
