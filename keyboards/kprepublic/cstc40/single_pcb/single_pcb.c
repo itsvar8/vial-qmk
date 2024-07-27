b083ba85bed83d1e70911a8e7122115860a929a4
Var <120035196+itsvar8@users.noreply.github.com>
2024-07-27 16:19:48 +0200

    Update single_pcb.c
    rgb layer indication on alpha keys

--- keyboards/kprepublic/cstc40/single_pcb/single_pcb.c
+++ keyboards/kprepublic/cstc40/single_pcb/single_pcb.c
@@ -69,3 +69,36 @@ const is31fl3733_led_t PROGMEM g_is31fl3733_leds[IS31FL3733_LED_COUNT] = {
     { 0, SW3_CS12, SW2_CS12, SW1_CS12 }
 };
 #endif
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+	for (uint8_t i = led_min; i < led_max; i++) {
+		if (g_led_config.flags[i] & LED_FLAG_MODIFIER) {
+			switch(get_highest_layer(layer_state)) {
+				case 1:
+					rgb_matrix_set_color(i, RGB_RED);
+					break;
+				case 2:
+					rgb_matrix_set_color(i, RGB_YELLOW);
+					break;
+				case 3:
+					rgb_matrix_set_color(i, RGB_BLUE);
+					break;
+				case 4:
+					rgb_matrix_set_color(i, RGB_MAGENTA);
+					break;
+				case 5:
+					rgb_matrix_set_color(i, RGB_TEAL);
+					break;
+				case 6:
+					rgb_matrix_set_color(i, RGB_ORANGE);
+					break;
+				case 7:
+					rgb_matrix_set_color(i, RGB_PURPLE);
+					break;
+				default:
+					break;
+			}
+		}
+    }
+	return false;
+}
